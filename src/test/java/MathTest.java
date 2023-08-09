def15c2774555def5ccecfcc0b14151099fb4f3c
Astra277 <joshua.m.banks@aexp.com>
2023-08-09 12:02:05 +0100

    Add unit testing

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MathTest {
+    @Test
+    public void isMathsReal() {
+        assertTrue(1+1==2, "either something has gone very wrong or your computer doesnt know basic maths");
+    }
+}
